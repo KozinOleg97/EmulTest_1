bdf27da28ff75a30c520dd1e55236e45267e8aca
Oleg Kosin <olegkosin@rambler.ru>
2018-11-09 18:32:34 +0300

    + загрузка в ОАМ2 из ОАМ при отрисовке линии

--- src/com/company/PPU.java
+++ src/com/company/PPU.java
@@ -22,11 +22,72 @@ public class PPU {
     //$3F10-$3F1F Спрайты
     //$3F20-$3FFF	$00E0	Mirrors of $3F00-$3F1F
 
+    //256x240
+
     private Byte[] PPUMemory;
-    private Byte[] OAM;
+    public Byte[] OAM;   //separate address space
+    public Byte[] OAM2;
+
+    private Byte[] paletteSprite, paletteBackground;
+
+
+    private boolean flagSizeOfSprite = true;
+
+    private Byte OAM2Index = 0;
 
     PPU() {
         PPUMemory = new Byte[64 * 256]; //16 Kb 16384 Byte
         OAM = new Byte[256]; //256 Byte; 4 Byte for sprite; sprites 8x8 or 8x16
+        OAM2 = new Byte[4 * 8];
+
+        paletteSprite = new Byte[16];
+        paletteBackground = new Byte[16];
+
     }
+
+
+    private void loadSpriteToOAM2(int spriteNumb) { // 1 спрайт из OAM в OAM2
+
+
+        for (int i = 0; i < 4; i++) {
+            OAM2[OAM2Index + i] = OAM[spriteNumb + i];
+        }
+
+    }
+
+
+    private void chooseSpritesToDraw(Byte line) { //заполняет oam2
+        Byte sizeOfSprite = 8;
+        if (!flagSizeOfSprite) {
+            sizeOfSprite = 16;
+        }
+
+        Integer nubm = 0;
+        for (int i = 0; i < 256; i = i + 4) {
+            if (line - OAM[i] < sizeOfSprite) {
+                nubm++;
+                if (nubm < 9) {
+                    loadSpriteToOAM2(i);
+                } else if (nubm == 9) {
+                    ///TODO set overflow flag
+                }
+
+            }
+
+        }
+
+    }
+
+    private void drawPixel() {
+
+    }
+
+
+    private Byte calcColor(int n, int x, int y) {
+
+
+        return null;
+    }
+
+
 }
